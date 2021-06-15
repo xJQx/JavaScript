f3a0a72e820302cba6e534eae3f60759578fa43b
xJQx <tohjingqiang@gmail.com>
2021-06-15 21:40:39 +0800

    caculator

---
+++ Calculator/static/index.js
@@ -0,0 +1,65 @@
+TypeList = ['addition', 'substract', 'multiply', 'divide'];
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector('#calculate').onclick = () => {
+        Calculate();
+    }
+})
+
+function Calculate() {
+    n1 = parseFloat(document.querySelector('#n1').value);
+    n2 = parseFloat(document.querySelector('#n2').value);
+    type = document.querySelector('#type').value;
+
+    // check for correct inputs type
+    check = false;
+
+    // check for type
+    for (i = 0; i < TypeList.length; i++)
+    {
+        if (TypeList[i] == type)
+        {
+            check = true;
+        }
+    }
+
+    // check for numbers
+    if (typeof(n1) !== 'number' || typeof(n2) !== 'number')
+    {
+        check = false;
+    }
+
+    if (!check)
+    {
+        document.querySelector("#wrong").style.display = 'block';
+        document.querySelector('#result').style.display = 'none';
+        return;
+    }
+    else
+    {
+        document.querySelector("#wrong").style.display = 'none';
+    }
+    
+    // find result
+    if (type == "addition")
+    {
+        result = n1 + n2;
+    }
+    else if (type == "substract")
+    {
+        result = n1 - n2;
+    }
+    else if (type == "divide")
+    {
+        result = n1 / n2;
+    }
+    else if (type == "multiply")
+    {
+        result = n1 * n2;
+    }
+
+    // display result
+    showResult = document.querySelector('#result');
+    showResult.style.display = 'block';
+    showResult.innerHTML = `Result: ${result}`;
+}
